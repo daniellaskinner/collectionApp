59103d13d8eaa67954c87f720e0b6282725b8c9f
daniellaskinner <daniellaskinner120@gmail.com>
2020-02-03 15:14:18 +0000

    created index file, styles.css file, added html boilerplate structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>;
+
+
+<html lang="en">
+<head>
+    <title>The Cheese Collection</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+    <h1>The Cheese Collection</h1>
+</body>
+</html>
