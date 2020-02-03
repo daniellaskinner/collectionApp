82182f9c46417449dcfe5f3a74e478588cb89a42
daniellaskinner <daniellaskinner120@gmail.com>
2020-02-03 15:29:47 +0000

    added title and navbar, styled with google fonts

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
 <?php
 
-?>;
+?>
 
 
 <html lang="en">
 <head>
     <title>The Cheese Collection</title>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" type="text/css" href="normalize.css">
     <link rel="stylesheet" type="text/css" href="styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Permanent+Marker&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <h1>The Cheese Collection</h1>
+    <nav>
+        <ul>
+            <li><a href="#collection">View Collection</li>
+            <li><a href="#form">Add a Cheese</li>
+        </ul>
+    </nav>
 </body>
 </html>
