fca91def6caa69e62d7e21059bede6a61ddd8ba1
daniellaskinner <daniellaskinner120@gmail.com>
2020-02-04 13:51:03 +0000

    changed naming of fields in db

--- DB/cheese_2020-02-04.sql
+++ DB/cheese_2020-02-04.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.29)
 # Database: cheese
-# Generation Time: 2020-02-03 14:43:17 +0000
+# Generation Time: 2020-02-04 13:47:04 +0000
 # ************************************************************
 
 
@@ -28,18 +28,23 @@ DROP TABLE IF EXISTS `cheese`;
 CREATE TABLE `cheese` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `Name` varchar(25) DEFAULT NULL,
-  `CountryOfOrigin` varchar(20) DEFAULT NULL,
-  `WinePairing` varchar(30) DEFAULT NULL,
-  `FunFact` varchar(200) DEFAULT NULL,
+  `Countryoforigin` varchar(20) DEFAULT NULL,
+  `Winepairing` varchar(30) DEFAULT NULL,
+  `Funfact` varchar(200) DEFAULT NULL,
+  `imgurl` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 LOCK TABLES `cheese` WRITE;
 /*!40000 ALTER TABLE `cheese` DISABLE KEYS */;
 
-INSERT INTO `cheese` (`id`, `Name`, `CountryOfOrigin`, `WinePairing`, `FunFact`)
+INSERT INTO `cheese` (`id`, `Name`, `Countryoforigin`, `Winepairing`, `Funfact`, `imgurl`)
 VALUES
-	(1,'Stilton','England','Lambrusco Dolce','Must be produced in Derbyshire, Leicestershire or Nottinghamshire.');
+	(1,'Stilton','England','Lambrusco Dolce','Must be produced in Derbyshire, Leicestershire or Nottinghamshire.','collectionApp/images/stilton.jpeg'),
+	(2,'Gouda','Netherlands','Malbec','One of oldest recorded cheeses in the world still made today.','collectionApp/images/gouda.jpeg'),
+	(3,'Mozarella','Italy','Dry Rosé','Often called \"the queen of the Mediterranean cuisine\", \"white gold\" or \"the pearl of the table?.','collectionApp/images/mozarella.jpeg'),
+	(4,'Brie','France','Chardonnay','Takes around 5-6 weeks to mature.','collectionApp/images/brie.jpg'),
+	(5,'Edam','Netherlands','Syrah','Edam is traditionally sold in rounded cylinders with a pale yellow interior and a coat, or rind, of red paraffin wax.','collectionApp/images/edam.jpeg');
 
 /*!40000 ALTER TABLE `cheese` ENABLE KEYS */;
 UNLOCK TABLES;
