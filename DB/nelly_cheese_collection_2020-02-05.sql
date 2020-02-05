d9b373aec8f6b99a7e80c976d6ade57578c32047
daniellaskinner <daniellaskinner120@gmail.com>
2020-02-05 16:06:46 +0000

    exported updated db

--- DB/nelly_cheese_collection_2020-02-05.sql
+++ DB/nelly_cheese_collection_2020-02-05.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.29)
 # Database: nelly_cheese_collection
-# Generation Time: 2020-02-05 15:34:07 +0000
+# Generation Time: 2020-02-05 16:06:20 +0000
 # ************************************************************
 
 
@@ -42,7 +42,7 @@ INSERT INTO `cheese` (`id`, `name`, `countryoforigin`, `winepairing`, `funfact`,
 VALUES
 	(1,'Stilton','England','Lambrusco Dolce','Must be produced in Derbyshire, Leicestershire or Nottinghamshire.','images/stilton.jpeg'),
 	(2,'Gouda','Netherlands','Malbec','One of oldest recorded cheeses in the world still made today.','images/gouda.jpeg'),
-	(3,'Mozarella','Italy','Dry Rose','Often called \"the queen of the Mediterranean cuisine\", \"white gold\" or \"the pearl of the table?.','images/mozarella.jpeg'),
+	(3,'Mozarella','Italy','Dry Rose','Often called \"the queen of the Mediterranean cuisine\", \"white gold\" or \"the pearl of the table\".','images/mozarella.jpeg'),
 	(4,'Brie','France','Chardonnay','Takes around 5-6 weeks to mature.','images/brie.jpg'),
 	(5,'Edam','Netherlands','Syrah','Edam is traditionally sold in rounded cylinders with a pale yellow interior and a coat, or rind, of red paraffin wax.','images/edam.jpeg');
 
